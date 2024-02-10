b81dbca9729434c9594829df06540137fb063498
Алексей Горощеня <lastpaladin13@gmail.com>
2024-02-10 15:05:30 +0300

    first commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,85 @@
-document.addEventListener('click', (e)=>{
-  if(e.target.closest('#filters-open') || e.target.closest('#filters-close') || e.target.closest('#filters-apply')){
-    document.querySelector('.filters__filters').classList.toggle('hidden')
+
+
+
+
+const blogList = document.querySelector('.blog__list')
+const scrollInput = document.querySelector('.custom-scrollbar')
+
+const changeScrollInput = (input) => {
+
+  let scrollSize = ((blogList.scrollHeight - blogList.clientHeight) / 100) * parseFloat(input.value)
+  blogList.scroll({
+    top: scrollSize,
+    left: 0,
+  })
+}
+
+
+const moveLanguageButton = () => {
+  if (window.screen.width < 992) {
+
+    const languageButton = document.querySelector('#lang').cloneNode(true);
+
+    document.querySelector('.header__menu-button ').before(languageButton)
+    document.querySelectorAll('#lang')[0].remove()
+  } else if ((window.screen.width >= 992)) {
+
+    const languageButton = document.querySelector('#lang').cloneNode(true);
+    document.querySelector('.header__buttons ').prepend(languageButton)
+    document.querySelectorAll('#lang')[1].remove()
+
   }
+}
 
-  if(!document.querySelector('.nav-header__list').classList.contains('hidden') && !e.target.closest('.nav-header__list')){
-      document.querySelector('.nav-header__list').classList.add('hidden')
-    }
+blogList.addEventListener('scroll', (e) => {
 
-  if(e.target.closest('#header-button') || e.target.closest('.nav-header__link')){
-    e.preventDefault()
-    document.querySelector('.nav-header__list').classList.toggle('hidden')
-  }
+  const rangepercent = Math.ceil((blogList.scrollTop / (blogList.scrollHeight - blogList.clientHeight)) * 100)
 
-  
-
-  if(document.querySelector('.header__input').classList.contains('hidden') && e.target.closest('.header__search svg')){
-    document.querySelector('.header__input').classList.toggle('hidden')
-  }
-   
-  if(!document.querySelector('.header__input').classList.contains('hidden') && !e.target.closest('.header__search')){
-    document.querySelector('.header__input').classList.add('hidden')
-  }
-
-  
+  scrollInput.value = rangepercent
 })
+
+scrollInput.addEventListener('input', (e) => {
+  changeScrollInput(e.target)
+})
+
+document.addEventListener('click', (e) => {
+  if (e.target.closest('.header__menu-button')) {
+    e.target.closest('.header__menu-button').classList.toggle('active')
+    document.querySelector('.header__menu').classList.toggle('active')
+  }
+
+  if (e.target.closest('#lang') && !e.target.closest('.buttons-header__language-option')) {
+    document.querySelector('.buttons-header__language-list').classList.toggle('active')
+  }
+
+  if(document.querySelector('.buttons-header__language-list.active') && !e.target.closest('#lang')){
+    document.querySelector('.buttons-header__language-list').classList.remove('active')
+  }
+
+  if (e.target.closest('.buttons-header__language-option')) {
+    document.querySelectorAll('.buttons-header__language-option').forEach(item => {
+      item.classList.remove('active')
+      e.target.closest('.buttons-header__language-option').classList.add('active')
+    })
+    document.querySelector('#lang span').textContent = e.target.textContent
+
+    if (window.screen.width < 992) {
+      let timeoutId = setTimeout(() => {
+      document.querySelector('.buttons-header__language-list').classList.remove('active')
+      clearTimeout(timeoutId)
+    }, 1000)
+    } else {}
+    
+   
+  }
+
+
+
+})
+
+window.addEventListener("orientationchange", function () {
+  moveLanguageButton()
+}, false);
+
+moveLanguageButton()
+
